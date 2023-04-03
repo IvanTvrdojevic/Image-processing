cd2e84cdf49d90e50236053c42f604037a2aac99
ivan <ivantvrdojevic@gmail.com>
2023-04-03 21:34:19 +0200

    removed debug + cleaned code

--- python/main.py
+++ python/main.py
@@ -48,44 +48,25 @@ def logTransformation(mat):
 
 def filter(mat, mask):
     resMat = mat.copy()
-    res = 0
     for i in range (4):
-        k = 0 if i > 0 else 1
-        tmpK = k
         aStart = i - 1 if i > 0 else 0
         aEnd = i + 1 if i < 3 else aStart + 1
-        print("as", aStart)
-        print("ae", aEnd)
         for j in range (4):
-            m = 0 if j > 0 else 1 
-            tmpM = m
             bStart = j - 1 if j > 0 else 0
             bEnd = j + 1 if j < 3 else bStart + 1
-            print("bs", bStart)
-            print("be", bEnd)
-            for a in range (aStart, aEnd + 1):
-                for b in range (bStart, bEnd + 1):
-                    print(".......................................................", aStart, " ", bStart, " ")
-                    print("mat", mat[a, b])
-                    print("mask", mask[k, m])
-                    print("k", k)
-                    print("m", m)
-                    res += mat[a, b] * mask[k, m]
-                    print("res", res)
-                    m += 1
-                m = tmpM
-                k += 1
-            print("end of one filtration\n\n\n")
-            k = tmpK
-            resMat[i, j] = res             
+            k = 0 if i > 0 else 1
             res = 0
+            for a in range (aStart, aEnd + 1):
+                m = 0 if j > 0 else 1 
+                for b in range (bStart, bEnd + 1):
+                    res += mat[a, b] * mask[k, m]
+                    m += 1
+                k += 1
+            resMat[i, j] = res             
     print("After run-lenght filtering")
     print(resMat)
     print("--------------------------------------------------------------------------", "\n")   
     
-
-
-
 def getGaussMask():
     pass
 
